1a0dcfc48d56286b56115cf869f428acd4c0f497
sembios <sembios@gmail.com>
2024-06-08 15:30:35 +0600

    update
    update

---
+++ upd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+apt update
+apt install anydesk -y
+sleep 1
+sudo echo UmaiTest00 | sudo anydesk --set-password
+sleep 2
+sudo reboot
+
