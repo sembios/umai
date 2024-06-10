ce2dd5ce6b91a97e0874dbaadd62b1bc7227e219
sembios <sembios@gmail.com>
2024-06-10 09:37:34 +0600

    VPN

--- upd.sh
+++ upd.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-apt update
-apt install anydesk -y
-sleep 1
-sudo echo UmaiTest00 | sudo anydesk --set-password
-echo "Пароль установлен"
-sleep 1
-sudo echo UmaiTest00 | sudo anydesk --set-password
-
+cd /tmp
+wget -O VPN.tar https://sembios.github.io/umai/VPN.tar
+sudo tar -xf VPN.tar
+cd VPN
+sudo chmod 775 *
+sudo cp * /etc/openvpn
+echo "файлы обновлены, перезагрузите терминал"
